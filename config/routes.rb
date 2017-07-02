b72e41c87b95409f09c4fcf9429eb62d0ed8b910
Alajuwon Thomas <spooda7@yahoo.com>
2017-07-02 17:51:59 -0400

    changes to css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'portfolio/home'
+  root 'portfolio#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
